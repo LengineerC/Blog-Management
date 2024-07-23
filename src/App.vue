0a0559467ca794fda005741e10cb91328ef89c8b
LengineerC <lengineerc@outlook.com>
2024-07-23 20:51:52 +0800

    create project and install dependencies

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Home from '@/views/Home.vue';
+
+</script>
+
+<template>
+  <Home />
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name:"Home",
+  data() {
+
+  },
+
+}
+</script>
+
+<template>
+  <div>
+    <el-button>123</el-button>
+  </div>
+</template>
